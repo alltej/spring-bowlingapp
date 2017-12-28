158294b77282d4628d8952cd4d9981715069a2c3
alltej <allan.tejano@gmail.com>
2017-12-28 06:37:55 -0500

    Remove the ScoreReader component. Change to use the stream...

--- src/main/java/com/alltej/bowling/BowlingApplication.java
+++ src/main/java/com/alltej/bowling/BowlingApplication.java
@@ -11,16 +11,18 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Component
+
 @SpringBootApplication
 public class BowlingApplication implements CommandLineRunner {
 
 	private final Logger logger = LoggerFactory.getLogger(BowlingApplication.class);
-
-	@Autowired
-	private ScoreReader scoreRetriever;
 
 	public static void main(String[] args) {
         SpringApplication app = new SpringApplication(BowlingApplication.class);
@@ -28,34 +30,26 @@ public class BowlingApplication implements CommandLineRunner {
         app.run(args);
 	}
 
-	@Override public void run( String... args ) {
-        if (args.length > 0) {
-			ArrayList<String[]> scores;
-			try {
-				String filePath = getFullPath( args[0] );
-				scores = scoreRetriever.getScores( filePath );
-				CreateBowlingMatch( scores );
-			} catch ( IOException e ) {
-				logger.error( String.valueOf( e ) );
-			}
+	@Override public void run( String... args ) throws IllegalArgumentException {
+		if (args.length <= 0)
+			throw new IllegalArgumentException( "Please provide input file" );
 
-        } else {
-			logger.error( "Please provide input file" );
-		}
+		try {
+            String filePath = new File(args[0]).getAbsolutePath();
+			List<String[]> scores = Files.readAllLines( Paths.get( filePath ), StandardCharsets.UTF_8 )
+				.stream().map( l -> l.split( "\\s+" ) )
+				.collect( Collectors.toList() );
+            CreateBowlingMatch( scores );
+        } catch ( IOException e ) {
+            logger.error( String.valueOf( e ) );
+        }
+
 	}
 
-	public String getFullPath( String filename ) {
-		String basePath = new File(filename).getAbsolutePath();
-		return basePath;
-	}
-
-	private void CreateBowlingMatch( ArrayList<String[]> list ) {
+	private void CreateBowlingMatch( List<String[]> list ) {
 		try {
 			BowlingMatch g = BowlingMatch.create();
-			for ( String[] s:
-                   list) {
-                g.roll( s[0], s[1] );
-            }
+			list.forEach( s -> g.roll( s[0], s[1] ) );
 			System.out.println(g.getPrintString());
 		} catch ( Exception e ) {
 			logger.error( e.getMessage() );
--- src/main/java/com/alltej/bowling/ScoreReader.java
+++ src/main/java/com/alltej/bowling/ScoreReader.java
@@ -8,21 +8,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Component
 public class ScoreReader {
 
     public ArrayList<String[]> getScores(String inputFile) throws IOException {
-        ArrayList<String[]> inputList = new ArrayList<>();
+        //ArrayList<String[]> inputList = new ArrayList<>();
 
         List<String> lines = Files.readAllLines( Paths.get(inputFile),
                 StandardCharsets.UTF_8);
 
-        for (String line : lines) {
-            String[] fields = line.trim().split("\\s+");
-            inputList.add( fields );
-        }
+        List<String[]> inputList = Files.readAllLines( Paths.get( inputFile ), StandardCharsets.UTF_8 )
+                .stream().map( l -> l.split( "\\s+" ) )
+                .collect( Collectors.toList() );
+//        for (String line : lines) {
+//            String[] fields = line.trim().split("\\s+");
+//            inputList.add( fields );
+//        }
 
-        return inputList;
+        return ( ArrayList<String[]> ) inputList;
     }
 }
